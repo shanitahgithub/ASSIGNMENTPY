963977698d20b66b68aadc841e261a924d3362d0
shanitahgithub <142230234+shanitahgithub@users.noreply.github.com>
2023-10-11 00:41:28 +1400

    ASSIGNMENT

---
+++ ex.py
@@ -0,0 +1,34 @@
+
+
+# Write a python function to multiply numbers in the list[8,2,3,-1,7]
+def multiplying_numbers(a,b,c,d,e):
+      output=[a*b*c*d*e]
+      print(output)
+multiplying_numbers(8,2,3,-1,7)
+
+
+# Write a python program to print only even numbers from the list[1,2,3,4,5,6,7,9]
+def even_numbers(a,b,c,d,e,f,g,i):
+      output=[b,d,f]
+      print(output)
+even_numbers(1,2,3,4,5,6,7,9)
+
+# Write a python function to sum all the numbers in a list [8,2,3,0,7]
+def sum_of_all_numbers(a,b,c,d,e):
+   output=a+b+c+d+e
+   print(output)
+sum_of_all_numbers(8,2,3,0,7)
+
+# Write a python program to reverse the string "1234abcd"
+def reversed_string(str):
+  for x in reversed(str):
+   print(x)
+reversed_string("1234abcd")
+
+
+
+
+
+
+   
+
